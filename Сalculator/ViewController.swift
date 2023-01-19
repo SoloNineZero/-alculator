58b42576a54c2d6a23600995c546aff4e0f3c08e
SoloNineZero <solodyankin.ie@ya.ru>
2023-01-19 11:32:39 +0500

    Added mathematical operation

--- Сalculator/ViewController.swift
+++ Сalculator/ViewController.swift
@@ -10,24 +10,54 @@ import UIKit
 class ViewController: UIViewController {
 
     var numberFromScreen: Double = 0
+    var firstNumber: Double = 0
+    var operation = 0
+    var mathSign: Bool = false
     
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func digits(_ sender: UIButton) {
-        resultLabel.text! += String(sender.tag)
+        if mathSign == true {
+            resultLabel.text = String(sender.tag)
+            mathSign = false
+        } else {
+            resultLabel.text! += String(sender.tag)
+        }
+        
         numberFromScreen = Double(resultLabel.text!)!
     }
     
     @IBAction func buttons(_ sender: UIButton) {
-        if resultLabel.text != "" {
-            switch button {
-            case sender.tag == 11:
-            case sender.tag == 12:
-            case sender.tag == 13:
-            case sender.tag == 14:
-            default:
-                sender.tag == 15
+        if resultLabel.text != "" && sender.tag != 10 && sender.tag != 15 {
+            firstNumber = Double(resultLabel.text!)!
+            if sender.tag == 11 {
+                resultLabel.text = "/"
+            } else if sender.tag == 12 {
+                resultLabel.text = "x"
+            } else if sender.tag == 13 {
+                resultLabel.text = "-"
+            } else if sender.tag == 14 {
+                resultLabel.text = "+"
+            } else if sender.tag == 15 {
+                resultLabel.text = "="
             }
+            operation = sender.tag
+            mathSign = true
+        } else if sender.tag == 15 {
+            if operation == 11 {
+                resultLabel.text = String(firstNumber / numberFromScreen)
+            } else if operation == 12 {
+                resultLabel.text = String(firstNumber * numberFromScreen)
+            } else if operation == 13 {
+                resultLabel.text = String(firstNumber - numberFromScreen)
+            } else if operation == 14 {
+                resultLabel.text = String(firstNumber + numberFromScreen)
+            }
+        } else if sender.tag == 10 {
+            resultLabel.text = ""
+            firstNumber = 0
+            numberFromScreen = 0
+            operation = 0
         }
     }
     
