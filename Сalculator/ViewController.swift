eac22db70e6fb930121e71254bd27e470fc91dec
SoloNineZero <solodyankin.ie@ya.ru>
2023-01-19 11:16:47 +0500

    Add functional

--- Сalculator/ViewController.swift
+++ Сalculator/ViewController.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numberFromScreen: Double = 0
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func digits(_ sender: UIButton) {
+        resultLabel.text! += String(sender.tag)
+        numberFromScreen = Double(resultLabel.text!)!
+    }
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if resultLabel.text != "" {
+            switch button {
+            case sender.tag == 11:
+            case sender.tag == 12:
+            case sender.tag == 13:
+            case sender.tag == 14:
+            default:
+                sender.tag == 15
+            }
+        }
+    }
+    
+     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
